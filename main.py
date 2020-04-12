054a9abd229094ca8af19de6b92364aba22cc4f2
jroeder5597 <jroeder1820@gmail.com>
2020-04-11 22:34:19 -0700

    appid request and loop created

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import steam.steamid
 import requests as r
 import json
 
+games = r.get('https://api.steampowered.com/ISteamApps/GetAppList/v1')
+games_json = games.json()
+games_json = (games_json['applist']['apps']['app'])
+
+game_arr = []
+counter = 0
+for i in range(len(games_json)):
+    game_arr.append(games_json[counter]['appid'])
+    counter = counter + 1
+
+
+
+
 test = r.get('http://store.steampowered.com/api/appdetails?appids=57690')
 
 output = test.json()
 print(str(output['57690']['data']["price_overview"]['final']))
+
