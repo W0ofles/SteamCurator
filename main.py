125b9b2528a0985c7b1710c76dd1d9846b37cdd6
jroeder5597 <jroeder1820@gmail.com>
2020-04-12 15:32:32 -0700

    Selenium interactivity added, need 2FA

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import steam.steamid
 import requests as r
 import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 games = r.get('https://api.steampowered.com/ISteamApps/GetAppList/v1')
 games_json = games.json()
@@ -21,3 +23,31 @@ for i in game_arr:
         print(game_query[str(i)]['data']["price_overview"]['final'])
     except:
         print('no price')
+
+
+
+
+
+driver = webdriver.Firefox()
+driver.get('https://store.steampowered.com/app/727020/Arcade_Moonlander_Plus/')
+test_web = driver.find_element_by_class_name("btn_addtocart").click()
+
+
+page = requests.get("https://isthereanydeal.com/#/")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+table = soup.find(class_="cntBoxContent")
+sales = table.find_all(class_="bundle-container-outer bundle-preview giveaway")
+#bundles = table.find_all(class_="bundle-container-outer bundle-preview bundle")
+links = table.find_all(class_="lg")
+
+
+test = []
+for i in links:
+    test.append(i.get('href'))
+
+str_list = (filter(None, test))
+str_list = list(set(str_list))
+
+for i in str_list:
+    print(i)
