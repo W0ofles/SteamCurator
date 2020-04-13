6c000cb3b0a48bd76377d688a08feb7897937bdb
jroeder5597 <jroeder1820@gmail.com>
2020-04-12 17:19:05 -0700

    authentication added, firefox is headless

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
-import steam.steamid
-import requests as r
-import json
+import steam.webauth as wa
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-
-games = r.get('https://api.steampowered.com/ISteamApps/GetAppList/v1')
-games_json = games.json()
-games_json = (games_json['applist']['apps']['app'])
-
-game_arr = []
-counter = 0
-for i in range(len(games_json)):
-    game_arr.append(games_json[counter]['appid'])
-    counter = counter + 1
+from selenium.webdriver.firefox.options import Options
 
 
+options = Options()
+options.add_argument('--headless')
 
-for i in game_arr:
-    try:
-        game_query = r.get('http://store.steampowered.com/api/appdetails?appids=' + str(i))
-        game_query = game_query.json()
-        print(game_query[str(i)]['data']["price_overview"]['final'])
-    except:
-        print('no price')
+username = input("Steam Username: ")
+password = input("Steam Password: ")
+mfa = input("Steam Authenticator Code: ")
 
-
-
-
-
-driver = webdriver.Firefox()
-driver.get('https://store.steampowered.com/app/727020/Arcade_Moonlander_Plus/')
-test_web = driver.find_element_by_class_name("btn_addtocart").click()
-
+user = wa.WebAuth(username, password)
+session = user.login(twofactor_code=mfa)
+session.get('https://steamcommunity.com')
 
 page = requests.get("https://isthereanydeal.com/#/")
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -41,13 +24,29 @@ sales = table.find_all(class_="bundle-container-outer bundle-preview giveaway")
 #bundles = table.find_all(class_="bundle-container-outer bundle-preview bundle")
 links = table.find_all(class_="lg")
 
-
-test = []
+tags = []
 for i in links:
-    test.append(i.get('href'))
+    tags.append(i.get('href'))
 
-str_list = (filter(None, test))
+str_list = (filter(None, tags))
 str_list = list(set(str_list))
 
+links_list = []
 for i in str_list:
-    print(i)
+    links_list.append(i)
+
+def SteamAccess(gameLink):
+    driver = webdriver.Firefox(options=options)
+    driver.get(gameLink)
+    for c in session.cookies :
+        driver.add_cookie({'name': c.name, 'value': c.value, 'path': c.path, 'expiry': c.expires})
+    driver.get(gameLink)
+    driver.add_cookie
+    try:
+        web = driver.find_element_by_class_name("btn_addtocart").click()
+    except:
+        pass
+    finally:
+        driver.quit()
+for i in links_list:
+    SteamAccess(i)
